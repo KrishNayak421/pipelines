f9d66a579866b24c070bfd554dc56ed502cf61c3
KrishNayak421 <krishnayak421@gmail.com>
2025-11-30 02:04:30 +0530

    Added CI Pipeline

---
+++ index.js
@@ -0,0 +1,4 @@
+console.log("Hello, World!");
+
+console.log("CI/CD using GitHub Actions");
+
---
+++ test.js
@@ -0,0 +1,5 @@
+console.log("Starting test pipeline...");
+
+setTimeout(() => console.log("Waiting 3 seconds..."), 3000);
+
+console.log("Test pipeline completed.");
